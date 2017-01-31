2d7a4cb5159c98ae9121c83794ca11464120dabf
Pablo Barberá <pablo.barbera@nyu.edu>
2017-01-31 09:36:53 -0800

    added unlist function

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,43 @@
 
 
 
+unlistWithNA <- function(lst, field){
+    if (length(field)==1){
+        notnulls <- unlist(lapply(lst, function(x) !is.null(x[[field]])))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], '[[', field))
+    }
+    if (length(field)==2){
+        notnulls <- unlist(lapply(lst, function(x) !is.null(x[[field[1]]][[field[2]]])))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], function(x) x[[field[1]]][[field[2]]]))
+    }
+    if (length(field)==3 & field[1]!="geo"){
+        notnulls <- unlist(lapply(lst, function(x) !is.null(x[[field[1]]][[field[2]]][[field[3]]])))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], function(x) x[[field[1]]][[field[2]]][[field[3]]]))
+    }
+    if (field[1]=="geo"){
+        notnulls <- unlist(lapply(lst, function(x) !is.null(x[[field[1]]][[field[2]]])))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], function(x) x[[field[1]]][[field[2]]][[as.numeric(field[3])]]))
+    }
+
+    if (length(field)==4 && field[2]!="urls"){
+        notnulls <- unlist(lapply(lst, function(x) length(x[[field[1]]][[field[2]]][[field[3]]][[field[4]]])>0))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], function(x) x[[field[1]]][[field[2]]][[field[3]]][[field[4]]]))
+    }
+    if (length(field)==4 && field[2]=="urls"){
+        notnulls <- unlist(lapply(lst, function(x) length(x[[field[1]]][[field[2]]])>0))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], function(x) x[[field[1]]][[field[2]]][[as.numeric(field[3])]][[field[4]]]))
+    }
+    if (length(field)==6 && field[2]=="bounding_box"){
+        notnulls <- unlist(lapply(lst, function(x) length(x[[field[1]]][[field[2]]])>0))
+        vect <- rep(NA, length(lst))
+        vect[notnulls] <- unlist(lapply(lst[notnulls], function(x) 
+            x[[field[1]]][[field[2]]][[field[3]]][[as.numeric(field[4])]][[as.numeric(field[5])]][[as.numeric(field[6])]]))
+    }
+    return(vect)
+}
